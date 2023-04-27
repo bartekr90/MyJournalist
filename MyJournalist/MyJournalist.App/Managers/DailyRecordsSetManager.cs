c22f3f4cd0b3770025fcb226ff8b1f8c5674bba1
Bartosz Rachwał <bartekr90@gmail.com>
2023-04-27 18:11:41 +0200

    Added manager clases for logic
    refers to issue #3

---
+++ MyJournalist/MyJournalist.App/Managers/DailyRecordsSetManager.cs
@@ -0,0 +1,81 @@
+﻿using MyJournalist.App.Abstract;
+using MyJournalist.Domain.Entity;
+
+namespace MyJournalist.App.Managers;
+
+public class DailyRecordsSetManager : IDailyRecordsSetManager
+{
+    private IDateTimeProvider _dateTimeProvider;
+    private IFileCollectionService<DailyRecordsSet> _fileService;
+    private IDailyRecordsSetService _dailySetService;
+
+    public DailyRecordsSetManager(IDateTimeProvider dateTimeProvider,
+                                  IFileCollectionService<DailyRecordsSet> fileService,
+                                  IDailyRecordsSetService dailySetService)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _fileService = fileService;
+        _dailySetService = dailySetService;
+    }
+
+    public Dictionary<(int year, int month), List<DailyRecordsSet>> GroupDailySetsByMonth(List<DailyRecordsSet> dailySets)
+    {
+        var dailySetsByMonth = dailySets
+            .GroupBy(drs => new { drs.RefersToDate.Year, drs.RefersToDate.Month });
+
+        var result = new Dictionary<(int year, int month), List<DailyRecordsSet>>();
+
+        foreach (var group in dailySetsByMonth)
+        {
+            result.Add((group.Key.Year, group.Key.Month), group.ToList());
+        }
+
+        return result;
+    }
+
+    public void LoadDailySetsFromFile(DateTimeOffset date)
+    {
+        _dailySetService.RefersToDate = date;
+
+        if (CheckFileExists())
+        {
+            var list = _fileService.ReadFile(_dailySetService.GetFileName()).ToList() ?? new List<DailyRecordsSet>();
+            _dailySetService.SetItems(list);
+        }
+        else
+            _fileService.CreateFile(_dailySetService.GetFileName());
+    }
+    private bool CheckFileExists()
+    {
+        return _fileService.CheckFileExists(_dailySetService.GetFileName());
+    }
+
+    public void SetDateForService(DateTimeOffset date)
+    {
+        _dailySetService.RefersToDate = date;
+    }
+
+    public DailyRecordsSet GetDailyRecordsSet(ICollection<Record>? records, ICollection<Tag>? tags)
+    {
+        if (!(records == null || records.Count == 0))
+            return _dailySetService.GetDailyRecordsSet(records, tags, _dateTimeProvider);
+        else
+            return _dailySetService.GetEmptyObj(_dateTimeProvider);
+    }
+
+    public void SaveListInFile(ICollection<DailyRecordsSet> setsFromMonth)
+    {
+        DailyRecordsSet? set = setsFromMonth.FirstOrDefault();
+        if (set == null)
+            return;
+
+        LoadDailySetsFromFile(set.RefersToDate);
+
+        List<DailyRecordsSet> setsToSave = _dailySetService.MakeUnion(_dailySetService.GetAllItems(), setsFromMonth);
+
+        _fileService.ClearFile(_dailySetService.GetFileName());
+
+        _fileService.UpdateFile(setsToSave, _dailySetService.GetFileName());
+    }
+}
+
---
+++ MyJournalist/MyJournalist.App/Managers/TagManager.cs
@@ -0,0 +1,76 @@
+﻿using MyJournalist.App.Abstract;
+using MyJournalist.Domain.Entity;
+
+namespace MyJournalist.App.Managers;
+
+public class TagManager : ITagManager
+{
+    private IFileCollectionService<Tag> _fileService;
+    private IDateTimeProvider _dateTimeProvider;
+    private ITagService _tagService;
+
+    public TagManager(IDateTimeProvider dateTimeProvider,
+                      IFileCollectionService<Tag> fileService,
+                      ITagService tagService)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _fileService = fileService;
+        _tagService = tagService;
+    }
+
+    public void MargeAndSaveTagsInFile(ICollection<Tag> tags)
+    {
+        LoadTagsFromFile();
+
+        List<Tag> newList = MergeTags(_tagService.GetAllItems(), tags);
+
+        _fileService.ClearFile(_tagService.GetFileName());
+
+        _fileService.UpdateFile(newList, _tagService.GetFileName());
+    }
+
+
+    public List<Tag> MergeTags(ICollection<Tag>? primaryTags, ICollection<Tag>? newTags)
+    {
+        return _tagService.MakeUnion(primaryTags, newTags);
+    }
+
+    public void LoadTagsFromFile()
+    {
+
+        if (CheckFileExists())
+        {
+            var list = _fileService.ReadFile(_tagService.GetFileName()).ToList() ?? new List<Tag>();
+            _tagService.SetItems(list);
+        }
+        else
+            _fileService.CreateFile(_tagService.GetFileName());
+    }
+
+    public List<Tag> GetTagsFromContent(string contentFromTxt)
+    {
+        uint tokens = _tagService.FindTokens(contentFromTxt);
+        List<Tag> tags = _tagService.FindAllTags(contentFromTxt, _dateTimeProvider, tokens);
+        return tags;
+    }
+
+    public List<Tag> MergeTagsFromRecordList(List<Record> records)
+    {
+        if (records == null || records.Count == 0)
+            return new List<Tag>();
+
+        List<Tag> primaryTags = records.FirstOrDefault().Tags?.ToList() ?? new List<Tag>();
+
+        foreach (Record record in records)
+        {
+            primaryTags = MergeTags(primaryTags, record.Tags);
+        }
+        return primaryTags;
+    }
+
+    private bool CheckFileExists()
+    {
+        return _fileService.CheckFileExists(_tagService.GetFileName());
+    }
+}
+

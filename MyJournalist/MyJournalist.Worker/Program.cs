b5f587a6e455ad4a2eea264a37e3eda7757f54dc
Bartosz Rachwał <bartekr90@gmail.com>
2023-04-27 18:26:41 +0200

    Added methods for main aplication classes

--- MyJournalist/MyJournalist.Worker/Program.cs
+++ MyJournalist/MyJournalist.Worker/Program.cs
@@ -1,9 +1,25 @@
+using MyJournalist.Domain.Entity;
 using MyJournalist.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddSingleton<ITxtFileService, TxtFileService>();
+
+        services.AddSingleton<IFileCollectionService<Record>, JsonFileService<Record>>();
+        services.AddTransient<IRecordService, RecordService>();
+        services.AddTransient<IRecordManager, RecordManager>();
+
+        services.AddSingleton<IFileCollectionService<DailyRecordsSet>, JsonFileService<DailyRecordsSet>>();
+        services.AddTransient<IDailyRecordsSetService, DailyRecordsSetService>();
+        services.AddTransient<IDailyRecordsSetManager, DailyRecordsSetManager>();
+
+        services.AddSingleton<IFileCollectionService<Tag>, JsonFileService<Tag>>();
+        services.AddTransient<ITagService, TagService>();
+        services.AddTransient<ITagManager, TagManager>();
     })
     .Build();
 
--- MyJournalist/MyJournalist.Worker/Worker.cs
+++ MyJournalist/MyJournalist.Worker/Worker.cs
@@ -1,21 +1,91 @@
-namespace MyJournalist.Worker
+using MyJournalist.Domain.Entity;
+
+namespace MyJournalist.Worker;
+
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private readonly ILogger<Worker> _logger;
+    private ITagManager _tagManager;
+    private IRecordManager _recordManager;
+    private IDailyRecordsSetManager _dailyRecordsSetManager;
+    private IDateTimeProvider _dateTimeProvider;
+
+    public Worker(ILogger<Worker> logger,
+                  ITagManager tagManager,
+                  IRecordManager recordManager,
+                  IDateTimeProvider dateTimeProvider,
+                  IDailyRecordsSetManager dailyRecordsSetManager)
     {
-        private readonly ILogger<Worker> _logger;
+        _logger = logger;
+        _tagManager = tagManager;
+        _recordManager = recordManager;
+        _dateTimeProvider = dateTimeProvider;
+        _dailyRecordsSetManager = dailyRecordsSetManager;
+    }
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        RunAllFilesCheck();
+        return base.StartAsync(cancellationToken);
+    }
 
-        public Worker(ILogger<Worker> logger)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _logger = logger;
+            RunTxtCheck();
+            await Task.Delay(15000, stoppingToken);
         }
+    }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        RunAllFilesCheck();
+        return base.StopAsync(cancellationToken);
+    }
+
+    private void RunTxtCheck()
+    {
+        string content = _recordManager.GetDataFromTxt();
+        List<Tag> tags = _tagManager.GetTagsFromContent(content);
+        _recordManager.SaveRecordInFile(_recordManager.GetRecord(content, tags));
+        _recordManager.ClearTxt();
+    }
+
+    private void RunAllFilesCheck()
+    {
+        List<Tag> tagsToSave = new List<Tag>();
+        string content = _recordManager.GetDataFromTxt();
+        List<Tag> tags = _tagManager.GetTagsFromContent(content);
+        List<Record> recordsList = _recordManager.MakeNewRecordList(_recordManager.GetRecord(content, tags));
+        List<Record> pastRecords = _recordManager.FindPastDateRecords(recordsList, _dateTimeProvider.Now);
+
+        if (!(pastRecords == null || pastRecords.Count == 0))
         {
-            while (!stoppingToken.IsCancellationRequested)
+            List<DailyRecordsSet> newDailyRecordsSetsList = new();
+            IEnumerable<IGrouping<DateTime, Record>> recordsGroupedByMonth = _recordManager.GroupRecordsByDate(pastRecords);
+
+            foreach (IGrouping<DateTime, Record> group in recordsGroupedByMonth)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                List<Record> fullRecords = _recordManager.GeRecordsWithContent(group.ToList());
+                _dailyRecordsSetManager.SetDateForService(group.Key);
+                List<Tag> tagsForDailySet = _tagManager.MergeTagsFromRecordList(fullRecords);
+                DailyRecordsSet dailyRecordSet = _dailyRecordsSetManager.GetDailyRecordsSet(fullRecords, tagsForDailySet);
+                newDailyRecordsSetsList.Add(dailyRecordSet);
+                tagsToSave = _tagManager.MergeTags(tagsToSave, tagsForDailySet);
+            }
+
+            var dailySetsGroupedByMonth = _dailyRecordsSetManager.GroupDailySetsByMonth(newDailyRecordsSetsList);
+
+            foreach (var group in dailySetsGroupedByMonth)
+            {
+                _dailyRecordsSetManager.SaveListInFile(group.Value);
             }
         }
+
+        List<Record> presentRecords = _recordManager.FindEqualDateRecords(recordsList, _dateTimeProvider.Now);
+        _recordManager.ClearTxt();
+        _recordManager.SaveListInFile(presentRecords);
+        _tagManager.MargeAndSaveTagsInFile(tagsToSave);
+
     }
 }

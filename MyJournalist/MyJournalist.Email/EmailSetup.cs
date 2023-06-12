cfd084e699423f456d809da7c9818be606c8ac1f
Bartosz Rachwał <bartekr90@gmail.com>
2023-06-12 13:06:19 +0200

    Added dependency injection setup added required settings

---
+++ MyJournalist/MyJournalist.Email/EmailSetup.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace MyJournalist.Email;
+
+public static class EmailSetup
+{
+    public static string HostSmtp { get; set; } = "localhost";
+    public static bool EnableSsl { get; set; } = false;
+    public static int Port { get; set; } = 25;
+    public static string SenderEmail { get; set; } = "defaultSenderEmail";
+    public static string SenderEmailPassword { get; set; } = "defaultSenderEmailPassword";
+    public static bool DefaultCredentials { get; set; } = true;
+   
+    public static bool LocalSettings { get; set; } = true;
+    public static string HostSmtpLocal { get; set; } = "localhost";
+    public static bool EnableSslLocal { get; set; } = false;
+    public static int PortLocal { get; set; } = 25;
+
+    public static SmtpClient GetClient()
+    {
+        if (LocalSettings)
+            return new SmtpClient()
+            {
+                Host = HostSmtpLocal,
+                EnableSsl = EnableSslLocal,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = PortLocal
+            };
+        else
+            return new SmtpClient()
+            {
+                Host = HostSmtp,
+                EnableSsl = EnableSsl,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = Port,
+                UseDefaultCredentials = DefaultCredentials,
+                Credentials = new NetworkCredential(SenderEmail, SenderEmailPassword)
+            };
+    }
+}
--- MyJournalist/MyJournalist.Worker/Program.cs
+++ MyJournalist/MyJournalist.Worker/Program.cs
@@ -3,11 +3,19 @@ using MyJournalist.App.Common;
 using MyJournalist.App.Concrete;
 using MyJournalist.App.Managers;
 using MyJournalist.Domain.Entity;
+using MyJournalist.Email;
 using MyJournalist.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((host, services) =>
     {
+        SetEmailSetup(host);
+
+        services
+        .AddFluentEmail(EmailSetup.SenderEmail)
+        .AddRazorRenderer()
+        .AddSmtpSender(EmailSetup.GetClient());
+
         services.AddHostedService<Worker>();
 
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
@@ -28,3 +36,44 @@ IHost host = Host.CreateDefaultBuilder(args)
     .Build();
 
 host.Run();
+
+static void SetEmailSetup(HostBuilderContext host)
+{
+    var emailSettings = host.Configuration.GetSection("EmailSettings");
+
+    int port;
+    if (!int.TryParse(emailSettings["Port"], out port))
+        port = 25;
+
+    int portLocal;
+    if (!int.TryParse(emailSettings["PortLocal"], out portLocal))
+        portLocal = 25;
+
+    bool enableSsl;
+    if (!bool.TryParse(emailSettings["EnableSsl"], out enableSsl))
+        enableSsl = false;
+
+    bool enableSslLocal;
+    if (!bool.TryParse(emailSettings["EnableSslLocal"], out enableSslLocal))
+        enableSslLocal = false;
+
+    bool localSettings;
+    if (!bool.TryParse(emailSettings["UseLocalSettings"], out localSettings))
+        localSettings = true;
+
+    bool defaultCredentials;
+    if (!bool.TryParse(emailSettings["UseDefaultCredentials"], out defaultCredentials))
+        defaultCredentials = false;
+
+    EmailSetup.HostSmtp = emailSettings["HostSmtp"] ?? "localhost";
+    EmailSetup.EnableSsl = enableSsl;
+    EmailSetup.Port = port;
+    EmailSetup.SenderEmail = emailSettings["SenderEmail"] ?? "defaultEmail";
+    EmailSetup.SenderEmailPassword = emailSettings["SenderEmailPassword"] ?? "defaultPassword";
+    EmailSetup.DefaultCredentials = defaultCredentials;
+    EmailSetup.LocalSettings = localSettings;
+    EmailSetup.HostSmtpLocal = emailSettings["HostSmtpLocal"] ?? "localhost";
+    EmailSetup.EnableSslLocal = enableSslLocal;
+    EmailSetup.PortLocal = portLocal;
+}
+
